0198eb8bd6335359f22c07590bca116822280e6f
Jagdish Saini <jsaini8941@gmail.com>
2021-10-03 23:57:37 +0530

    stocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [initial, setInitial] = useState<number>();
+  const [quantity, setQuantity] = useState<number>();
+  const [current, setPrice] = useState<number>();
+  const [output, setOutPut] = useState("");
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    if (Number(initial) > Number(current)) {
+      var loss = (Number(initial) - Number(current)) * Number(quantity);
+      var lossPercentage = (loss / Number(initial)) * 100;
+
+      setOutPut(
+        `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+      );
+    } else if (Number(current) > Number(initial)) {
+      var profit = (Number(current) - Number(initial)) * Number(quantity);
+      var profitPercentage = (profit / Number(initial)) * 100;
+
+      setOutPut(
+        `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+      );
+    } else {
+      setOutPut(`No pain no gain and no gain no pain`);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stock Profile & Loss Calculator</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            <label className="input-label">Initial Price</label>
+          </div>
+          <input
+            className="input"
+            type="number"
+            value={initial}
+            onChange={(e) => setInitial(e.target.value as unknown as number)}
+          />
+        </div>
+        <div>
+          <div>
+            <label className="input-label">Initial Price</label>
+          </div>
+          <input
+            className="input"
+            type="number"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value as unknown as number)}
+          />
+        </div>
+        <div>
+          <div>
+            <label className="input-label">Initial Price</label>
+          </div>
+          <input
+            className="input"
+            type="number"
+            value={current}
+            onChange={(e) => setPrice(e.target.value as unknown as number)}
+          />
+        </div>
+        <div>
+          <button className="btn-check" onClick={handleSubmit}>
+            Tell Me!!
+          </button>
+        </div>
+      </form>
+      <h1>{output}</h1>
     </div>
   );
 }
